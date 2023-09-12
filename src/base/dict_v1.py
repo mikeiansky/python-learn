7330aae4513e43288a0cbf50767bc4980c8b243f
mikeian <zhouwenxiang@ciwei.net>
2023-09-12 09:43:00 +0800

    线程对象使用

---
+++ src/base/dict_v1.py
@@ -0,0 +1,16 @@
+"""
+字典类型数据
+"""
+
+
+data = {
+    'name' : 'mike',
+    'age' : 35
+}
+
+print(data['name'])
+print(data['age'])
+
+data['address'] = 'china'
+
+print(data)
--- src/thread_v1/thread_v1_2.py
+++ src/thread_v1/thread_v1_2.py
@@ -8,16 +8,26 @@ import time
 age = 30
 tag = 'mike'
 
-def hello(msg, sleep):
+def hello(msg, sleep, age=None, address=None):
     time.sleep(sleep)
     print('thread name ', threading.get_ident())
     print('hello-', msg, ' , age:', age, ', tag:', tag)
+    print('current thread ', threading.current_thread())
+    print('native id', threading.current_thread().native_id)
+    print('is daemon thead', threading.current_thread().daemon)
+    print('main thread : ' , threading.main_thread())
+    print('time out max :', threading.TIMEOUT_MAX)
+    print('age :', age, ', address :', address)
+    # 不能将当前线程自己加入进去
+    # threading.current_thread().join()
 
 # 这里只有主线程激活
 print('1 thread active count' , threading.active_count())
 
-t1 = threading.Thread(target=hello, args=('t1', 1))
-t2 = threading.Thread(target=hello, args=('t2', 2))
+t1 = threading.Thread(target=hello, args=('t1', 1), 
+                      kwargs={'age':25, 'address':'china'}, daemon=True)
+t2 = threading.Thread(target=hello, args=('t2', 2), 
+                      kwargs={'age':27, 'address':'america'})
 # t3 = threading.Thread(target=hello, args=('t3', 1))
 
 # 这里只有主线程激活
