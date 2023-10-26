5e1ae1a58d2dc7f00a308b44049e15a95e244b4e
mikeian <zhouwenxiang@ciwei.net>
2023-10-26 14:33:33 +0800

    machine learning

--- src/numpy_v1/numpy_base.py
+++ src/numpy_v1/numpy_base.py
@@ -2,23 +2,8 @@
 numpy 基础
 """
 
-import numpy as np
-from numpy import newaxis
-import matplotlib.pyplot as plt
-
-
-a = np.arange(5)
-
-b = np.arange(15).reshape(5, 3)
-
-print('a')
-print(a)
-print('b')
-print(b)
-
-c = np.dot(a, b)
-print('c')
-print(c)
+Y = [0] * 20 + [1] * 20
+print(type(Y))
 
 
 
